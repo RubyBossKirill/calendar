e78e2d3465b9d9ba5cdb0182c393d8050c979ca5
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-11-02 18:48:47 +0300

    new js

--- calendar.js
+++ calendar.js
@@ -1,22 +1,3 @@
 console.log("calendar.js загружен и выполняется");
 
-const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exechttps://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exec';
-
-async function fetchEvents() {
- console.log("Функция fetchEvents запущена");
- try {
-     const response = await fetch(GOOGLE_SCRIPT_URL);
-     const data = await response.json();
-     console.log("Данные получены:", data);
-     return data.GoogleSheetData;
- } catch (error) {
-     console.error("Ошибка при загрузке событий:", error);
-     return [];
- }
-}
-
-// Пример инициализации календаря для теста
-document.addEventListener('DOMContentLoaded', () => {
- console.log("DOM загружен, инициализация календаря начинается");
- // Вызов функций для отображения календаря
-});
+/* const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exechttps://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exec';*/
