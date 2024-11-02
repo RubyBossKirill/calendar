f28513741fad7c6673168004881c9c5bf5ad1629
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-11-02 18:52:09 +0300

    new js

--- calendar.js
+++ calendar.js
@@ -1,3 +1,15 @@
+/* const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exechttps://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exec';*/
+
 console.log("calendar.js загружен и выполняется");
 
-/* const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exechttps://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exec';*/
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("DOM загружен и обработчик событий запущен");
+
+    const calendarContainer = document.getElementById('color-calendar');
+    if (calendarContainer) {
+        calendarContainer.textContent = "Календарь успешно загружен";
+        console.log("Контейнер календаря найден и текст добавлен");
+    } else {
+        console.error("Контейнер календаря не найден!");
+    }
+});
