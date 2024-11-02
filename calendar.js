c0cd68f947ab8f22e13de0a720bba6faf8d47553
RubyBossKirill <160587634+RubyBossKirill@users.noreply.github.com>
2024-11-02 18:40:11 +0300

    new js

--- calendar.js
+++ calendar.js
@@ -1,3 +1,5 @@
+console.log("calendar.js загружен и выполняется");
+
 const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exechttps://script.google.com/macros/s/AKfycbxhLNqZcw1ccAMnNjMSXyJZXgfKnAUgDd1QhatKUEj3oi_Fbv2G-KQ_33QhP7P4U0arXA/exec';
 
 async function fetchEvents() {
